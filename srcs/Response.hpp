5d5cbcbdb529db94108a099dc63b2c4b89c7ed30
clorin <cyrille.lorin@yahoo.fr>
2022-01-06 14:12:25 +0100

    ADD: Pages errors en dur

--- srcs/Response.hpp
+++ srcs/Response.hpp
@@ -31,6 +31,10 @@
 #include "Request.hpp"
 #include "Server.hpp"
 
+# define PAGE403    "<h1>Forbidden</h1><p>You don't have permission to access this resource.</p>"
+# define PAGE404    "<h1 style='color:red'>Error 404: Not found :-(</h1><p>I have no idea where that file is, sorry.  Are you sure you typed in the correct URL?</p>"
+# define PAGE500    "<h1>Oops! Something went wrong...</h1><p>We seem to be having some technical difficulties. Hang tight.</p>"
+
 class Response
 {
 
