884c1be742821816a6a47254549a4fa78dda9ea2
clorin <cyrille.lorin@yahoo.fr>
2022-01-06 19:08:39 +0100

    ADD to FIX: Custom_ERROR.hpp

---
+++ srcs/Custom_ERROR.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Custom_ERROR.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 14:15:42 by clorin            #+#    #+#             */
+/*   Updated: 2022/01/06 14:16:36 by clorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUSTOM_ERROR_HPP
+# define CUSTOM_ERROR_HPP
+
+# define PAGE403    "<h1>Forbidden</h1><p>You don't have permission to access this resource.</p>"
+# define PAGE404    "<h1 style='color:red'>Error 404: Not found :-(</h1><p>I have no idea where that file is, sorry.  Are you sure you typed in the correct URL?</p>"
+# define PAGE500    "<h1>Oops! Something went wrong...</h1><p>We seem to be having some technical difficulties. Hang tight.</p>"
+
+
+#endif
